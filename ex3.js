9808fa5840938d0f9cf753416520623d398d60ca
hakeem91 <hakeem91@hotmail.fr>
2017-01-11 05:00:42 -0500

    first commit

---
+++ ex3.js
@@ -0,0 +1,11 @@
+function rentrez(){
+  var date;
+  date =prompt("Rentrez votre date de naissance");
+  
+  if(date<=31){
+    alert('Format de date valide');
+  }
+  else
+    alert('Veuillez rentrer une date valide');
+}
+rentrez();
